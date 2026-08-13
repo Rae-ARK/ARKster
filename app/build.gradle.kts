872c7410f0d347e75820c295cd1f603be7e4fcca
Rae ARK <horizonarkstudio@gmail.com>
2026-08-13 14:29:25 +0530

    Fix bugs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,11 +29,6 @@ android {
     }
 }
 
-repositories {
-    google()
-    mavenCentral()
-}
-
 dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2023.12.00")
     implementation(composeBom)
